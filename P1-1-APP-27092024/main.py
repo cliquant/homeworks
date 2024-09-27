8d52bb47c6c382f1fd5d427c97da81b851415259
Cliquant <42009476+cliquant@users.noreply.github.com>
2024-09-27 12:38:03 +0300

    Update main.py

--- P1-1-APP-27092024/main.py
+++ P1-1-APP-27092024/main.py
@@ -1,1 +1,15 @@
+print("P1-1 Niks A.")
 
+try:
+    user = input("Ievadi skaitli, lai aprēķinātu: ")
+    r = None
+    try:
+        r = float(user)
+        pi = 3.14
+        c = 2 * pi * r
+        print(f"Atbilde: {c}")
+    except ValueError:
+        print("Lūdzu ievadi valid skaitli lai aprēķinātu.")
+
+except Exception as e:
+    print(f"Neizdevās aprēķināt: {r}")

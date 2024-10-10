bf063cc8908bd6e9a1a86aac52bf5dbfa9b900a1
Niks A <42009476+cliquant@users.noreply.github.com>
2024-10-10 23:04:49 +0300

    Update exc-4.py

--- P1-1-APP-27092024/exc-4.py
+++ P1-1-APP-27092024/exc-4.py
@@ -1,20 +1,11 @@
 print("P1-1 Niks A.")
 
+try:
+    num1 = float(input("Ievadiet pirmo skaitli: "))
+    num2 = float(input("Ievadiet otro skaitli: "))
+    num3 = float(input("Ievadiet trešo skaitli: "))
 
-def average():
-    try:
-        num1 = float(input("Ievadiet pirmo skaitli: "))
-        num2 = float(input("Ievadiet otro skaitli: "))
-        num3 = float(input("Ievadiet trešo skaitli: "))
-
-        average = (num1 + num2 + num3) / 3
-        return average
-    except ValueError:
-        print("Kļūda: Lūdzu ievadiet derīgus skaitļus.")
-        return None
-
-
-end = average()
-
-if end is not None:
-    print(f"Vidējā aritmētiskā vērtība ir: {end}")
+    average = (num1 + num2 + num3) / 3
+    print(f"Vidējā aritmētiskā vērtība ir: {average}")
+except ValueError:
+    print("Kļūda: Lūdzu ievadiet derīgus skaitļus.")

4d6f167d4bfb5370ce31f3060fad5cb8f5452450
Niks A <42009476+cliquant@users.noreply.github.com>
2024-10-10 22:58:42 +0300

    Update exc-3.py

--- P1-1-APP-27092024/exc-3.py
+++ P1-1-APP-27092024/exc-3.py
@@ -1,15 +1,8 @@
 print("P1-1 Niks A.")
 
-def htm():
-    try:
-        n = float(input("Ievadiet stundu skaitu: "))
-        minutes = n * 60
-        return minutes
-    except ValueError:
-        print("Kļūda: Lūdzu ievadiet derīgu skaitli.")
-        return None
-
-minutes = htm()
-
-if minutes is not None:
-    print(f"{minutes} minūtes ir {minutes / 60} stundās.")
+try:
+    n = float(input("Ievadiet stundu skaitu: "))
+    m = n * 60
+    print(f"{m} minūtes ir {m / 60} stundās.")
+except ValueError:
+    print("Kļūda: Lūdzu ievadiet derīgu skaitli.")

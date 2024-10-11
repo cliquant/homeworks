ada5f67a9931ea00868109feabf3405b7b49f35f
Niks A <42009476+cliquant@users.noreply.github.com>
2024-10-11 10:40:56 +0300

    Create exc-1.py

---
+++ P1-1-APP-11102024/exc-1.py
@@ -0,0 +1,17 @@
+import math
+
+print("P1-1 Niks A.\n")
+
+try:
+    x = float(input("Lūdzu, ievadiet x vērtību: "))
+
+    if x > 0:
+        y = math.sqrt(3 * x * 2)**3 - (24 * x) / (3 * math.sqrt(x)) - (2 / math.sqrt(x))
+    else:
+        y = (2 * x) - (3 - x) / 4
+
+    print(f"Kad x = {x}, y = {y:.2f}")
+except ValueError:
+    print("Lūdzu, ievadiet derīgu skaitli.")
+except ZeroDivisionError:
+    print(f"Nevar aprēķināt y, kad x = {x} dēļ dalīšanas ar nulli.")
